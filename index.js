284ab341a88285e3e17ddb830efc82b41b7e419c
carlitos321 <ca.cabrera321@gmail.com>
2017-11-14 23:44:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+  customerName.toUpperCase();
+  console.log(`the customer name is ${customerName}:`)
+}
+
+
+function setBestCustomer() {
+  if (customerName = "bob") {
+    return "not bob"
+  } else {
+    return "it is Bob!"
+  }
+}
+
+setBestCustomer(customerName);
