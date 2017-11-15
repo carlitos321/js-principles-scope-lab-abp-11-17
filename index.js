6ec1f1011ab779cc3be8ca6daaa628b7f9443b20
carlitos321 <ca.cabrera321@gmail.com>
2017-11-15 00:05:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var customerName = "bob"
 
 function upperCaseCustomerName() {
-  customerName.toUpperCase();
+  var customerName = customerName.toUpperCase();
   console.log(`the customer name is ${customerName}:`)
 }
 
